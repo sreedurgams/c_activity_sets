dbb1e6f8888629332070046e53392212ce7f0034
sreedurgams <141307693+sreedurgams@users.noreply.github.com>
2024-01-23 19:22:39 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+
+int main()
+{
+    int a, b, c, largest;
+
+    a = input();
+    b = input();
+    c = input();
+
+    largest = compare(a, b, c);
+
+    output(a, b, c, largest);
+
+    return 0;
+}
+
+int input()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    return num;
+}
+
+int compare(int a, int b, int c)
+{
+    if (a > b && a > c)
+        return a;
+    else if (b > a && b > c)
+        return b;
+    else
+        return c;
+}
+
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest of %d, %d, and %d is %d\n", a, b, c, largest);
+}
 
