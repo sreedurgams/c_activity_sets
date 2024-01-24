cc87d98c3cdcc84d03b69b3943be3f05076e5ba9
sreedurgams <141307693+sreedurgams@users.noreply.github.com>
2024-01-24 10:15:57 +0530

    test

--- set01/problem06.c
+++ set01/problem06.c
@@ -23,9 +23,9 @@ int input(int *a, int *b, int *c)
 
 void compare(int a, int b, int c, int *largest)
 {
-    if (a > b && a > c)
+    if (a >= b && a >= c)
         *largest = a;
-    else if (b > a && b > c)
+    else if (b >= a && b >= c)
         *largest = b;
     else
         *largest = c;
