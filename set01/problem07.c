79d8c8034b18d07e418dce6f78dc504058d09204
sreedurgams <141307693+sreedurgams@users.noreply.github.com>
2024-01-24 22:05:36 +0530

    test

--- set01/problem07.c
+++ set01/problem07.c
@@ -32,5 +32,5 @@ int sum_n_nos(int n)
 
 void output(int n, int sum)
  {
-    printf("The sum of all natural numbers until %d is **%d**.\n", n, sum);
+    printf("The sum of all natural numbers until %d is %d\n", n, sum);
 }
