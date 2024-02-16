03a6471a0dd48f0ff9dbb15418ebb0fe0cb588ea
sreedurgams <141307693+sreedurgams@users.noreply.github.com>
2024-02-16 13:51:41 +0530

    test

---
+++ set02/problem01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int input_side() {
+    int side;
+    printf("Enter the length of a side: ");
+    scanf("%d", &side);
+    return side;
+}
+
+int check_scalene(int a, int b, int c) {
+    if (a != b && b != c && a != c)
+        return 1; 
+    else
+        return 0;
+}
+
+void output(int a, int b, int c, int isscalene) {
+    printf("Side a: %d\n", a);
+    printf("Side b: %d\n", b);
+    printf("Side c: %d\n", c);
+    if (isscalene)
+        printf("The triangle with sides %d, %d, and %d is a scalene triangle.\n", a, b, c);
+    else
+        printf("The triangle with sides %d, %d, and %d is not a scalene triangle.\n", a, b, c);
+}
+
+int main() {
+    int a, b, c;
+    int isscalene;
+
+    a = input_side();
+    b = input_side();
+    c = input_side();
+
+    isscalene = check_scalene(a, b, c);
+    output(a, b, c, isscalene);
+
+    return 0;
+}
